7b88fc4f1f098c7304ae0a9214d264899d8c1c74
Tobias Herber <40146720150275@litec.ac.at>
2018-02-23 18:13:23 +0100

    add main package

---
+++ packages/vxv/index.js
@@ -0,0 +1,22 @@
+const stylis = require('stylis');
+const assembleTemplate = require('assemble-template');
+
+const hash = require('vxv-hash');
+const insert = require('vxv-insert');
+const parser = require('vxv-parser');
+const state = require('vxv-state');
+
+const vxv = (strings, ...values) => {
+  strings = Array.isArray(strings) ? strings : [strings];
+  let str = assembleTemplate(strings, ...values);
+
+  const h = hash(str);
+  const styles = parser(`.vxv_${h}`, str);
+
+  state.set(h, str);
+  insert(styles);
+
+  return `vxv_${h}`;
+};
+
+module.exports = vxv;
