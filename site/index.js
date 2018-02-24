6fb5255f8532b2b85aec0eade6d7078ae9658696
Tobias Herber <40146720150275@litec.ac.at>
2018-02-24 11:32:33 +0100

    prettify site

--- site/index.js
+++ site/index.js
@@ -2,38 +2,39 @@ const css = require('vxv');
 const html = require('xou');
 
 const navStyles = css`
-position: fixed;
-border-bottom: solid black 2px;
-top: 0px;
-right: 0px;
-left: 0px;
-padding: 10px 5px;
-font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-background: white;
+  position: fixed;
+  border-bottom: solid black 2px;
+  top: 0px;
+  right: 0px;
+  left: 0px;
+  padding: 10px 5px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+    'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  background: white;
 
-& .right {
-  float: right;
-}
-
-& a {
-  color: black;
-  text-decoration: none;
-  margin: 0px 15px;
-
-  &:hover {
-    text-decoration: underline;
+  & .right {
+    float: right;
   }
-}
 
-@media only screen and (max-width: 720px) {
-  & .other {
-    display: none
+  & a {
+    color: black;
+    text-decoration: none;
+    margin: 0px 15px;
+
+    &:hover {
+      text-decoration: underline;
+    }
   }
-}
+
+  @media only screen and (max-width: 720px) {
+    & .other {
+      display: none;
+    }
+  }
 `;
 
 const nav = html`
-  <nav class="${ navStyles }">
+  <nav class="${navStyles}">
     <span class="left"><a href="/">VXV</a></span>
     <span class="right">
       <a class="other" href="#usage">Usage</a>
@@ -44,30 +45,31 @@ const nav = html`
 `;
 
 const mainStyles = css`
-font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-max-width: 720px;
-margin: 150px auto 100px auto;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+    'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  max-width: 720px;
+  margin: 150px auto 100px auto;
 
-& a {
-  color: black;
-  text-decoration: none;
+  & a {
+    color: black;
+    text-decoration: none;
 
-  &:hover {
-    text-decoration: underline;
+    &:hover {
+      text-decoration: underline;
+    }
   }
-}
 
-& img {
-  width: 95%;
-  max-width: 250px;
-  display: block;
-  margin: auto;
-}
+  & img {
+    width: 95%;
+    max-width: 250px;
+    display: block;
+    margin: auto;
+  }
 
-& .sub {
-  text-align: center;
-  margin: 40px auto 50px auto;
-}
+  & .sub {
+    text-align: center;
+    margin: 40px auto 50px auto;
+  }
 `;
 
 const usageCode = `const vxv = require('vxv');
@@ -118,7 +120,7 @@ server();
 // => that you can use for serverside rendering.`;
 
 const main = html`
-  <section class="${ mainStyles }">
+  <section class="${mainStyles}">
     <img src="https://i.imgur.com/R4GAGr8.png" />
     <p class="sub">
       A tiny library for writing native css code in JavaScript.<br><b>VXV</b> is powered by <a href="https://github.com/thysultan/stylis.js">stylis</a>, a fast css preprocessor.
@@ -139,18 +141,18 @@ const main = html`
 
     <h2 id="usage">Usage</h2>
     <p><strong>VXV</strong> supports standard css(with some extras). It returns a simple class name that you can use to access the styles.</p>
-    <pre><code class="language-javascript">${ usageCode }</code></pre>
+    <pre><code class="language-javascript">${usageCode}</code></pre>
 
     <h3><a id="ssr"></a>Serverside rendering</h3>
     <p>For serverside rendering you need the <a href="https://github.com/herber/vxv/tree/master/packages/vxv-server">vxv-server</a> module.</p>
     <p><strong>VXV-Server</strong> processes your styles just like <strong>VXV</strong> does including hash prefixing. <code>server()</code> will return a simple string containing all your styles - you can now save those styles somewhere or send them directly to the user.</p>
-    <pre><code class="language-javascript">${ ssrCode }</code></pre>
+    <pre><code class="language-javascript">${ssrCode}</code></pre>
   </section>
 `;
 
 const node = html`<div>
-  ${ nav }
-  ${ main }
+  ${nav}
+  ${main}
 </div>`;
 
 document.body.appendChild(node);
