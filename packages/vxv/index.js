8592fcc571bce844072404577f2e32960695e2ff
Tobias Herber <40146720150275@litec.ac.at>
2018-02-24 11:18:26 +0100

    add nested styles

--- packages/vxv/index.js
+++ packages/vxv/index.js
@@ -5,9 +5,23 @@ const insert = require('vxv-insert');
 const parser = require('vxv-parser');
 const state = require('vxv-state');
 
+const buildString = (strings, ...values) => {
+  let str = '';
+
+  for (let i in strings) {
+    if (String(values[i]).startsWith('vxv_')) {
+      str += state.all()[values[i].substring(4)];
+    } else {
+      str += strings[i] + (values[i] !== undefined ? values[i] : '');
+    }
+  }
+
+  return str;
+};
+
 const vxv = (strings, ...values) => {
   strings = Array.isArray(strings) ? strings : [strings];
-  let str = assembleTemplate(strings, ...values);
+  const str = buildString(strings, ...values);
 
   const h = hash(str);
   const styles = parser(`.vxv_${h}`, str);
